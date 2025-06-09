64c3256b222a590cbc22dfa7080f054401bd7ade
Bhavya Gaur <hi2bhavya@gmail.com>
2025-06-10 01:07:02 +0530

    'all_corrected'

--- MEDIUM LEETCODE/198 House Robber/198 Space Optimal.cpp
+++ MEDIUM LEETCODE/198 House Robber/198 Space Optimal.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        int two_ahead = 0;  // dp[i+2]
-        int one_ahead = 0;  // dp[i+1]
-
+        int two_ahead = 0;
+        int one_ahead = 0;
         for (int i = n - 1; i >= 0; i--) {
             int pick = nums[i] + two_ahead;
             int skip = one_ahead;
             int curr = max(pick, skip);
-
-            // shift window
             two_ahead = one_ahead;
             one_ahead = curr;
         }
-
         return one_ahead;
     }
 };
+int main()
+{
+    vector<int>eg;
+    // initialise eg first
+    Solution obj1;
+    return obj1.rob(eg);
+}
